ccc6de6e2d51388d75c984da4ac996e9fceca3fe
alielabdo <alloyali48@gmail.com>
2025-02-20 03:26:27 +0200

    updated title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color1, setColor1] = useState('#ff8bff');
+  const [color2, setColor2] = useState('#ff0000');
+
+  const setGradient = () => {
+    return `linear-gradient(to right, ${color1}, ${color2})`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: setGradient() }}>
+      <h3>Current CSS Background</h3>
+      <div>
+        <label htmlFor="color1">Color 1:</label>
+        <input
+          type="color"
+          id="color1"
+          value={color1}
+          onChange={(e) => setColor1(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="color2">Color 2:</label>
+        <input
+          type="color"
+          id="color2"
+          value={color2}
+          onChange={(e) => setColor2(e.target.value)}
+        />
+      </div>
+      <h3>{setGradient()}</h3>
     </div>
   );
 }
